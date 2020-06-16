a95f5c2d2f3ad289ff0876831a67f61c331ddb29
USAMI Kenta <tadsan@zonu.me>
2020-06-17 01:42:07 +0900

    Add (emacs "24.3") to Package-Requires

--- composer.el
+++ composer.el
@@ -7,7 +7,7 @@
 ;; Version: 0.1.1
 ;; Keywords: tools php dependency manager
 ;; Homepage: https://github.com/zonuexe/composer.el
-;; Package-Requires: ((emacs "24") (cl-lib "0.5") (s "1.9.0") (f "0.17") (seq "1.9") (php-runtime "0.1.0"))
+;; Package-Requires: ((emacs "24.3") (s "1.9.0") (f "0.17") (seq "1.9") (php-runtime "0.1.0"))
 
 ;; This file is NOT part of GNU Emacs.
 
